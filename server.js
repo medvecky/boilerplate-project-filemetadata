1e7be5b23b63dc23e4910406fc124d28c6c26128
Glitch (fcc-file-metadata-serv) <none>
2020-04-29 06:47:35 +0000

    🍄💠 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+const multer = require('multer');
+const upload = multer().single('upfile');
 
 var app = express();
 
@@ -13,6 +15,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse', upload, (req, res) => {
+  if (req.file) {
+    const { originalname: name, mimetype: type, size } = req.file;
+    res.json({
+      name,
+      type,
+      size
+    });
+  } else {
+    res.json({
+      errror: 'no file selected'
+    });
+  }
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
